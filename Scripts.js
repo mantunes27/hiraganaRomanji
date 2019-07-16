7b3a6eabe064725941fa039c44dca6f096a51ce2
mantunes27 <marcoantunes86@gmail.com>
2019-07-16 16:49:45 +0100

    Scrapped MVC method for this. Only gonna use simple HTML and JS for it

---
+++ Scripts.js
@@ -0,0 +1,7 @@
+var hirganaVowels = ["あ","い","う","え" ,"お"];
+var latinVowels = ["a","i","u","e","o"];
+
+
+//Create array for english and another array for hirgana
+//Create sokuon method (double consonent) っ
+b,d,g,h,k,m,n,p,r,s,t,y,w,z
