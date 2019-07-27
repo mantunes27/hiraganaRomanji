a7f04e90e5d45075702ad635805a40c0230e6d73
mantunes27 <marcoantunes86@gmail.com>
2019-07-27 17:46:22 +0100

    Added full alphabet and script for normal Hiragana > Romanji Works
    //TO DO Sukuon logic

--- Scripts.js
+++ Scripts.js
@@ -1,15 +1,11 @@
-var hiraganaVowels = ["あ","い","う","え" ,"お"];
-var latinVowels = ["a","i","u","e","o"];
-var hiraganaConsonants = ["ば","び","ぶ","べ","ぼ","だ","ぢ","づ","で","ど","が","ぎ","ぐ","げ","ご","は","ひ","ふ","へ","ほ"];
-var latinConsonants = ["ba","bi","bu","be","bo","da","di","du","de","do","ga","gi","gu","ge","go","ha","hi","fu","he","ho"];
+var hiraganaAlphabet = ["あ","い","う","え" ,"お","ば","び","ぶ","べ","ぼ","だ","ぢ","づ","で","ど","が","ぎ","ぐ","げ","ご","は","ひ","ふ","へ","ほ","か","き","く","け","こ","ま","み","む","め","も","ん","ぱ","ぴ","ぷ","ぺ","ぽ","ら","り","る","れ","ろ","さ","し","す","せ","そ","た","ち","つ","て","と","や","ゆ","よ","わ","ゐ","ゑ","を","ざ","じ","ず","ぜ","ぞ"];
+var latinAlphabet = ["a","i","u","e","o","ba","bi","bu","be","bo","da","di","du","de","do","ga","gi","gu","ge","go","ha","hi","fu","he","ho","ka","ki","ku","ke","ko","ma","mi","mu","me","mo","n","pa","pi","pu","pe","po","ra","ri","ru","re","ro","sa","si","su","se","so","ta","ti","tu","te","to","ya","yu","yo","wa","wi","we","wo","za","zi","zu","ze","zo"];
 
 var wordOutput = "";
 //dummy data for now
-var wordInput = "っあああ";
+var wordInput = "あか";
 
-//Create array for english and another array for hiragana
 //Create sokuon method (double consonent) っ
-//k,m,n,p,r,s,t,y,w,z
 
 for(i = 0; i < wordInput.length; i++)
     {
@@ -18,14 +14,17 @@ for(i = 0; i < wordInput.length; i++)
     }
     else
     {
-         wordOutput += translate(wordInput,i);
+         wordOutput += translate(wordInput[i]);
     }
 }
 
+console.log(wordOutput);
 
 
-function translate(wordInput){
-    
+
+function translate(char){
+    var index = hiraganaAlphabet.indexOf(char);
+    return latinAlphabet[index];
 }
 
 function sokuonProcessing(wordInput,i){
