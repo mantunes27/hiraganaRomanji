9314f4e6059dca490784effa7b606e181bc271a0
mantunes27 <marcoantunes86@gmail.com>
2019-08-08 16:48:18 +0100

    Hiragana sanitizer input

--- Scripts.js
+++ Scripts.js
@@ -8,6 +8,12 @@ function hiraganaTranslator(){
     var hiraganaAlphabet = ["あ","い","う","え" ,"お","ば","び","ぶ","べ","ぼ","だ","ぢ","づ","で","ど","が","ぎ","ぐ","げ","ご","は","ひ","ふ","へ","ほ","か","き","く","け","こ","ま","み","む","め","も","ん","ぱ","ぴ","ぷ","ぺ","ぽ","ら","り","る","れ","ろ","さ","し","す","せ","そ","た","ち","つ","て","と","や","ゆ","よ","わ","ゐ","ゑ","を","ざ","じ","ず","ぜ","ぞ"];
     var latinAlphabet = ["a","i","u","e","o","ba","bi","bu","be","bo","da","di","du","de","do","ga","gi","gu","ge","go","ha","hi","fu","he","ho","ka","ki","ku","ke","ko","ma","mi","mu","me","mo","n","pa","pi","pu","pe","po","ra","ri","ru","re","ro","sa","si","su","se","so","ta","ti","tu","te","to","ya","yu","yo","wa","wi","we","wo","za","zi","zu","ze","zo"];
     
+    for(i = 0; i < wordInput.length; i++){
+        if(!hiraganaAlphabet.includes(wordInput[i])){
+            document.getElementById("demo").innerHTML = "Character " + wordInput[i] + "is not valid";
+            return false;
+        }
+    }
     var wordOutput = "";
     //dummy data for now
     //var wordInput = "あっか";
